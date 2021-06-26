ced0810bc0f8b69bff91281d477a28f7fc0717c2
Петр Хабухаев <habpete@mail.ru>
2021-06-26 16:59:39 +0300

    add view

--- sql/views.sql
+++ sql/views.sql
@@ -1,1 +1,4 @@
-CREATE VIEW public.events_view
+CREATE VIEW events_view AS
+    SELECT e.id, mt.typename, u.firstname, u.secondname, u.lastname, u.phone, u.email, e.created_time FROM public.events AS e
+    LEFT JOIN public.users AS u ON u.id = e.user_id
+    LEFT JOIN public.message_types AS mt ON mt.id = e.messagetype_id
